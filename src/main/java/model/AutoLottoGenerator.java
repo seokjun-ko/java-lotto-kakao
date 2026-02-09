fec1d72693ad162d846915817f88167d10be95ab
tjrwns1021@gmail.com <tjrwns1021@gmail.com>
2026-02-09 18:13:42 +0900

    Test: 로또 생성기가 생성한 로또 숫자는 6개여야 한다.

---
+++ src/main/java/model/AutoLottoGenerator.java
@@ -0,0 +1,9 @@
+package model;
+
+import java.util.List;
+
+public class AutoLottoGenerator {
+	public List<Integer> makeLotto() {
+		return List.of(1, 2, 3, 4, 5, 6);
+	}
+}
---
+++ src/test/java/TestAutoLottoGenerator.java
@@ -0,0 +1,12 @@
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+import model.AutoLottoGenerator;
+
+public class TestAutoLottoGenerator {
+	@Test
+	void 자동으로_생성된_로또번호는_6자리이다() {
+		AutoLottoGenerator generator = new AutoLottoGenerator();
+		assertThat(generator.makeLotto().size()).isEqualTo(6);
+	}
+}
