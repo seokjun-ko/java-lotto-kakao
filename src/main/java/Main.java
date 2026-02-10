2edae24705ac4d31b9d5f249a78110cbaddb8cba
zhongdan-b <128568951+ZhongdanBae@users.noreply.github.com>
2026-02-10 18:20:43 +0900

    feat: 입출력 기능 구현 완료

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,25 +8,28 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-
-		System.out.printf("구입금액을 입력해 주세요.");
 		Buyers buyer = new Buyers();
+		try {
+			System.out.printf("구입금액을 입력해 주세요.");
+			if(!sc.hasNextLine()) return;
+			int money = Integer.parseInt(sc.nextLine().trim());
+			Lottos lottos = buyer.buyLotto(money);
+			Lottos.printLottoTickets(lottos);
 
-		int money = sc.nextInt();
-		Lottos lottos = buyer.buyLotto(money);
+			System.out.printf("지난 주 당첨 번호를 입력해 주세요.");
+			if(!sc.hasNextLine()) return;
+			StoreResult store = new StoreResult();
+			store.setNumber(sc.nextLine());
 
-		Lottos.printLottoTickets(lottos);
+			System.out.printf("보너스 볼을 입력해 주세요.");
+			if(!sc.hasNextLine()) return;
+			store.setBonus(sc.nextLine());
 
-		System.out.printf("지난 주 당첨 번호를 입력해 주세요.");
-		StoreResult store = new StoreResult();
-		String numbers = sc.nextLine();
-		store.setNumber(numbers);
-		//
-		// System.out.printf("보너스 볼을 입력해 주세요.");
-		// String bonus = sc.nextLine();
-		// store.setBonus(bonus);
-
-
-
+			buyer.setResult(store.getResult());
+			buyer.compare();
+		}
+		catch (RuntimeException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
--- src/main/java/model/Buyers.java
+++ src/main/java/model/Buyers.java
@@ -43,6 +43,7 @@ public class Buyers {
         }
         for(Lotto lotto : lottos.getLottoList()) lottoResult.add(calculateRank(lotto));
         profitRate = (double) lottoResult.calculateTotalPrize() / spentMoney;
+        printWinningStatistics();
         return lottoResult.getResultCount();
     }
 
@@ -60,6 +61,22 @@ public class Buyers {
         int matchCount = 0;
         for(Integer number : lotto.getNumbers()) matchCount += result.getNumbers().contains(number) ? 1 : 0;
         return matchCount;
+    }
+
+    private void printWinningStatistics(){
+        if(profitRate == 0.0) throw new RuntimeException("총 수익률은 0이 될 수 없습니다.");
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        System.out.println("3개 일치 (" + LottoRank.FIFTH.getPrize() + "원)- " + lottoResult.getCount(LottoRank.FIFTH) + "개");
+        System.out.println("4개 일치 (" + LottoRank.FOURTH.getPrize() + "원)- " + lottoResult.getCount(LottoRank.FOURTH) + "개");
+        System.out.println("5개 일치 (" + LottoRank.THIRD.getPrize() + "원)- " + lottoResult.getCount(LottoRank.THIRD) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치(" + LottoRank.SECOND.getPrize() + "원) - " + lottoResult.getCount(LottoRank.SECOND) + "개");
+        System.out.println("6개 일치 (" + LottoRank.FIRST.getPrize() + "원)- " + lottoResult.getCount(LottoRank.FIRST) + "개");
+        if(profitRate > 1) {
+            System.out.printf("총 수익률은 %.2f입니다. 이득보셨습니다! 로또 짱짱%n", profitRate);
+            return;
+        }
+        System.out.printf("총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)%n", profitRate);
     }
 
 }
